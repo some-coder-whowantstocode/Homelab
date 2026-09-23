53261444b5177b512240fe6ad44a9b44fa29f472
some-coder-whowantstocode <rohit19003@gmail.com>
2026-09-24 01:30:41 +0530

    UPDATE: containerize homelab

--- server/internal/api/service.go
+++ server/internal/api/service.go
@@ -9,7 +9,7 @@ import (
 
 func Service(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 
 	system.ServiceMU.RLock()
--- server/internal/api/status.go
+++ server/internal/api/status.go
@@ -9,7 +9,7 @@ import (
 
 func Status(w http.ResponseWriter, r *http.Request) {
 
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 
 	system.StatMU.RLock()
